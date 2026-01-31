833085cf9c57dd672a7e3dde11463764d1aefa65
am-prasad <amprasad18@gmail.com>
2026-01-31 10:36:46 +0530

    backend CORS updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="VeriSense")
 origins = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
-    "https://verisense.onrender.com"
+    "https://verisense-frontend.onrender.com"
 ]
 
 app.add_middleware(
